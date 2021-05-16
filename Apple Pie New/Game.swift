cafcb11dad48fc8e23cc083c8d72f8cd03fea339
Berezentsev Evgeny <jonn09@icloud.com>
2021-05-16 17:21:22 +0300

    Initial Commit

---
+++ Apple Pie New/Game.swift
@@ -0,0 +1,36 @@
+//
+//  Game.swift
+//  Apple Pie New
+//
+//  Created by Евгений Березенцев on 06.02.2021.
+//
+
+import Foundation
+
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word {
+            if guessedLetters.contains(letter) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    
+
+}
